cbbbecf1a58c6d2048858a1ddbf11aafc1971752
Waz2010 <33770173+Waz2010@users.noreply.github.com>
2024-02-14 13:41:03 -0800

    Add files via upload
    This is initial commit

---
+++ Deck52/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Deck52
+{
+    public static class StrHelper
+    {
+        public static string ChangeFirstLatterCase(this string inputString)
+        {
+            if(inputString.Length > 0)
+            {
+                char[] CharArrays = inputString.ToCharArray();
+                CharArrays[0] = char.IsUpper(CharArrays[0])? char.ToLower(CharArrays[0]) : char.ToUpper(CharArrays[0]);
+                return new string(CharArrays);
+            }
+            return inputString;
+        }
+    }
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Deck> FUllDeck = new List<Deck>();
+            FUllDeck.CreateDeck();
+            FUllDeck.ShowCards();
+            FUllDeck.Shuffle();
+            FUllDeck.ShowCards();
+            FUllDeck = FUllDeck.ResetDeck();
+            FUllDeck.ShowCards();
+            Console.ReadLine();
+        }
+    }
+
+    public class Deck 
+    {
+        public int Sr { get; set; }
+        public string Card { get; set; }
+        public string Suite { get; set; }
+    }
+
+    public static class DeckHelper
+    {
+        public static void CreateDeck(this List<Deck> deck1)
+        {
+            deck1.AddSuites("Clubs");
+            deck1.AddSuites("Dimonds");
+            deck1.AddSuites("Hearts");
+            deck1.AddSuites("Spades");
+        }
+
+        private static void AddSuites(this List<Deck> p, string SuiteName)
+        {
+            p.Add(new Deck { Sr = p.Count + 1, Card = "ACE",   Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "Two",   Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "Three", Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "Four",  Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "Five",  Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "Six",   Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "Seven", Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "Eight", Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "Nine",  Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "Ten",   Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "Joker", Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "Queen", Suite = SuiteName });
+            p.Add(new Deck { Sr = p.Count + 1, Card = "king",  Suite = SuiteName });
+        }
+    }
+
+    public static class MySuffule
+    {
+        private static Random rng = new Random();
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        public static void ShowCards(this List<Deck> list)
+        {
+            Console.WriteLine("====================Show Cards started======================");
+            int k = 0;
+            foreach (var i in list)
+            {
+                
+                Console.WriteLine(k + " = " + i.Sr + " - " + i.Card + "- Of " + i.Suite);
+                k = k + 1;
+            }
+            Console.WriteLine("====================Show Cards Ended=======================");
+        }
+        public static List<Deck> ResetDeck(this List<Deck> list)
+        {
+            return list.OrderBy(x => x.Sr).ToList();
+        }
+    }
+}
+
+
+
+
+
+//string strName = string.Empty;
+//var ress = strName.ChangeFirstLatterCase();
+//var ress1 = StrHelper.ChangeFirstLatterCase(strName);
+//Console.WriteLine(ress);
---
+++ Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+
+    public delegate int myDelegate(int num1, int uum2);
+    public delegate int myDelegateeven(int[] arr);
+    public static int jj = 0;
+    public static int evennumb(int[] myarr)
+    {
+            return myarr.Where(i => i % 2 == 0).Sum(i => i);
+    }
+
+    public static int calculatesum(int x, int y)
+    {
+        return x + y;
+    }
+    public static DateTime time;
+    public static void Main()
+    {
+        string s1 = "like for example $  you don't have $  network $  access";
+        Console.WriteLine("before: {0}", s1);
+
+        GroupCollection halves = Regex.Match(s1, @"([^$]*\$)(.*)").Groups;
+        string answer = halves[1].Value + halves[2].Value.Replace("$", "");
+
+        Console.WriteLine("after: {0}", answer);
+
+        string[] Names = { "Sheraz Haider", "waseem aider", "waqar ider", "shehzad der " };
+        var FindWaseem = from namee in Names where !namee.EndsWith("JO") select namee;
+        int[] ar = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 10 };
+        myDelegateeven d1 = new myDelegateeven(evennumb);
+
+        int ressss = d1(ar);
+        /* ... */
+        if (time == null)
+        {
+            Console.WriteLine("Time is null");
+        }
+
+        //StringBuilder sb = new StringBuilder("Testthis")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // "20" is capacity 
+        StringBuilder s = new StringBuilder("HELLO ", 3);
+
+        s.Append("GFG");
+
+        // after printing "GEEKS" 
+        // a new line append 
+        s.AppendLine("GEEKS");
+
+        s.Append("GeeksForGeeks");
+        Console.WriteLine(s);
+
+        myDelegate d = new myDelegate(calculatesum);
+
+        var res = d(5, 6);
+
+        Console.WriteLine(res);
+
+        int TotalCoffeeCost = 0;
+        NewOrder:
+        Console.WriteLine("Please Enter 1 - for Small, 2 - for Medium, 3 - for Large...");
+        int CoffeeCupSize = int.Parse(Console.ReadLine());
+
+        switch (CoffeeCupSize)
+        {
+            case 1: TotalCoffeeCost += 1; break;
+            case 2: TotalCoffeeCost += 2; break;
+            case 3: TotalCoffeeCost += 3; break;
+            default:          
+                Console.WriteLine("Your Choice is invalid.. Please 1 - for Small, 2 - for Medium, 3 - for Large... ");goto NewOrder;
+        }
+
+        DecideAnotherCup:
+        Console.WriteLine("You want another cup of coffee.. Yes or NO?");
+        string AnotherCupofCoffee = Console.ReadLine().ToUpper();
+
+        do {
+            if (AnotherCupofCoffee != "YES" && AnotherCupofCoffee != "NO")
+            {
+                Console.WriteLine("Your choice {0} is invalid. Please try again...", AnotherCupofCoffee);
+                goto DecideAnotherCup;
+            }
+            else if (AnotherCupofCoffee.Equals("NO"))
+            {
+                goto Billing;
+            }
+            else 
+            {
+                goto NewOrder;
+            }
+                
+        } while (AnotherCupofCoffee.Equals("YES"));
+
+        //switch (AnotherCupofCoffee.ToUpper());
+        //{
+        //    case "YES":goto NewOrder;
+        //    case "NO": goto Billing;
+        //    default: Console.WriteLine("Your choice {0} is invalid. Please try again...", AnotherCupofCoffee);
+        //        goto DecideAnotherCup; 
+        //}
+
+        Billing:
+        Console.WriteLine("Thank you for shopping with us!");
+        Console.WriteLine("Bill amount is = {0}", TotalCoffeeCost);
+
+
+
+
+        //do
+        //{
+        //    Console.WriteLine("Enter number... 1 , 2 or 3");
+        //    int Mynumber = int.Parse(Console.ReadLine());
+
+        //}while(Mynumber == 3)
+
+
+        Class2 C1 = new Class2("Waseem","Haider",null);
+
+       
+
+
+
+
+
+        Console.ReadLine();
+
+    }
+
+}
+
+
+/// <summary>
+/// This is to understand null collasing
+/// </summary>
+public class Class2
+{
+    string _FName;
+    string _LName;
+    int? _NumberOfStudent;
+
+    /// <summary>
+    /// This is to understand null collasing 
+    /// </summary>
+    /// <param name="FName"  takes int ></param>
+    /// <param name="LName" takes int ></param>
+    /// <param name="NumberOfStudent" takes int ></param>
+    public Class2(string FName, string LName, int? NumberOfStudent)
+    {
+        _FName = FName;
+        _LName = LName;
+        _NumberOfStudent = NumberOfStudent;
+        _NumberOfStudent = _NumberOfStudent ?? 0;
+        Console.WriteLine(_FName + ":" + _LName + " : " + _NumberOfStudent);
+    }
+
+}
+
